e358b627d2dbb4ab48b0e163431371e8bba90057
Parham Taheri <parhamoyan@yahoo.com>
2022-04-23 00:14:24 +0200

    refactor and add type annotation

--- ReloadButton-01/src/MainWindow.py
+++ ReloadButton-01/src/MainWindow.py
@@ -4,18 +4,21 @@
 # All rights reserved.
 #
 # ///////////////////////////////////////////////////////////////
+from typing import Optional
 
 from Ui_MainWindow import *
 
+
 class MainWindow(QMainWindow):
-    def __init__(self):
-        QMainWindow.__init__(self)
+    def __init__(self, parent: Optional[QWidget] = None) -> None:
+        QMainWindow.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.center()
-    
-    def center(self):
+
+    def center(self) -> None:
         screen = QGuiApplication.screenAt(QCursor().pos())
         fg = self.frameGeometry()
         fg.moveCenter(screen.geometry().center())
         self.move(fg.topLeft())
+
--- ReloadButton-01/src/ReloadButton.py
+++ ReloadButton-01/src/ReloadButton.py
@@ -4,19 +4,18 @@
 # All rights reserved.
 #
 # ///////////////////////////////////////////////////////////////
+from typing import Optional
 
-from PySide6.QtCore import *
-from PySide6.QtGui import *
-from PySide6.QtWidgets import *
+from PySide6.QtCore import (QEasingCurve, QVariantAnimation,
+                            QSequentialAnimationGroup)
+from PySide6.QtGui import (QColor, Qt, QPainterPath, QPainter,
+                           QBrush, QPen, QFont, QPaintEvent)
+from PySide6.QtWidgets import QPushButton, QWidget
+
 
 class ReloadButton(QPushButton):
-    def __init__(
-            self,
-            parent = None,
-            animationDuration = 1000,
-            animationEasingCurve = QEasingCurve.InOutSine
-            ):
-        QPushButton.__init__(self, parent=parent)
+    def __init__(self, parent: Optional[QWidget] = None) -> None:
+        QPushButton.__init__(self, parent)
         self.setFixedSize(240, 100)
         self.setCursor(Qt.PointingHandCursor)
 
@@ -25,75 +24,75 @@ class ReloadButton(QPushButton):
         self.iconColor = QColor("#094873")
         self.length = 6
         self.currentPercentage = .8
-        self.animationDuration = animationDuration
-        self.animationEasingCurve = animationEasingCurve
+        self.animationDuration = 1000
+        self.animationEasingCurve = QEasingCurve.InOutSine
 
         self.clicked.connect(self.startAnimations)
-    
-    def enterEvent(self, e):
+
+    def enterEvent(self, e) -> None:
         self.backgroundColor = QColor("#dafcdb")
 
-    def leaveEvent(self, e):
+    def leaveEvent(self, e) -> None:
         self.backgroundColor = QColor("#fefefe")
-    
-    def initHideArrowAnimation(self):
-        self.hideArrowAnimation = QVariantAnimation(self)
-        self.hideArrowAnimation.setDuration(.2*self.animationDuration)
-        self.hideArrowAnimation.setStartValue(6)
-        self.hideArrowAnimation.setEndValue(0)
-        self.hideArrowAnimation.setEasingCurve(self.animationEasingCurve)
-        self.hideArrowAnimation.valueChanged.connect(self.updateLength)
 
-    def initTransitionAnimation(self):
-        self.transitionAnimation = QVariantAnimation(self)
-        self.transitionAnimation.setDuration(.6*self.animationDuration)
-        self.transitionAnimation.setStartValue(self.currentPercentage)
-        self.transitionAnimation.setEndValue(self.currentPercentage-.5)
-        self.transitionAnimation.setEasingCurve(self.animationEasingCurve)
-        self.transitionAnimation.valueChanged.connect(self.updatePercentage)
+    def getHideArrowAnimation(self) -> QVariantAnimation:
+        hideArrowAnimation = QVariantAnimation(self)
+        hideArrowAnimation.setDuration(int(.2 * self.animationDuration))
+        hideArrowAnimation.setStartValue(6)
+        hideArrowAnimation.setEndValue(0)
+        hideArrowAnimation.setEasingCurve(self.animationEasingCurve)
+        hideArrowAnimation.valueChanged.connect(self.updateLength)
+        return hideArrowAnimation
 
-    def initShowArrowAnimation(self):
-        self.showArrowAnimation = QVariantAnimation(self)
-        self.showArrowAnimation.setDuration(.2*self.animationDuration)
-        self.showArrowAnimation.setStartValue(0)
-        self.showArrowAnimation.setEndValue(6)
-        self.showArrowAnimation.setEasingCurve(self.animationEasingCurve)
-        self.showArrowAnimation.valueChanged.connect(self.updateLength)
-        
-    def startAnimations(self):
-        self.initHideArrowAnimation()
-        self.initTransitionAnimation()
-        self.initShowArrowAnimation()
-        self.seqGroup = QSequentialAnimationGroup(self)
-        self.seqGroup.addAnimation(self.hideArrowAnimation)
-        self.seqGroup.addAnimation(self.transitionAnimation)
-        self.seqGroup.addAnimation(self.showArrowAnimation)
-        self.seqGroup.start()
-    
-    def updateLength(self, newValue):
+    def getTransitionAnimation(self) -> QVariantAnimation:
+        transitionAnimation = QVariantAnimation(self)
+        transitionAnimation.setDuration(int(.6 * self.animationDuration))
+        transitionAnimation.setStartValue(self.currentPercentage)
+        transitionAnimation.setEndValue(self.currentPercentage - .5)
+        transitionAnimation.setEasingCurve(self.animationEasingCurve)
+        transitionAnimation.valueChanged.connect(self.updatePercentage)
+        return transitionAnimation
+
+    def getShowArrowAnimation(self) -> QVariantAnimation:
+        showArrowAnimation = QVariantAnimation(self)
+        showArrowAnimation.setDuration(int(.2 * self.animationDuration))
+        showArrowAnimation.setStartValue(0)
+        showArrowAnimation.setEndValue(6)
+        showArrowAnimation.setEasingCurve(self.animationEasingCurve)
+        showArrowAnimation.valueChanged.connect(self.updateLength)
+        return showArrowAnimation
+
+    def startAnimations(self) -> None:
+        seqGroup = QSequentialAnimationGroup(self)
+        seqGroup.addAnimation(self.getHideArrowAnimation())
+        seqGroup.addAnimation(self.getTransitionAnimation())
+        seqGroup.addAnimation(self.getShowArrowAnimation())
+        seqGroup.start()
+
+    def updateLength(self, newValue: int) -> None:
         self.length = newValue
         self.update()
-    
-    def updatePercentage(self, newPercentage):
+
+    def updatePercentage(self, newPercentage: float) -> None:
         self.currentPercentage = newPercentage
         if self.currentPercentage < 0:
             self.currentPercentage += 1
         if self.currentPercentage > 1:
             self.currentPercentage -= 1
         self.update()
-    
-    def trim(self, path, start):
+
+    @staticmethod
+    def trim(self, path: float, start: float) -> QPainterPath:
         newPath = QPainterPath()
-        newPath.moveTo(path.pointAtPercent(start))
+        newPath.moveTo(int(path.pointAtPercent(start)))
         for i in range(1, 101):
-            newPath.lineTo(path.pointAtPercent(start))
-            start += .38/100
+            newPath.lineTo(int(path.pointAtPercent(start)))
+            start += .38 / 100
             if start > 1:
                 start -= 1
         return newPath
-        
-    
-    def paintEvent(self, e):
+
+    def paintEvent(self, event: QPaintEvent) -> None:
         painter = QPainter(self)
         painter.setRenderHint(QPainter.Antialiasing)
         painter.setBrush(QBrush(self.backgroundColor))
@@ -109,18 +108,20 @@ class ReloadButton(QPushButton):
 
         path = QPainterPath()
         w, h = 20, 40
-        x, y = (100-w)/2, (self.height()-h)/2
-        path.addRoundedRect(x, y, w, h, w/2, w/2)
-        
+        x, y = (100 - w) / 2, (self.height() - h) / 2
+        path.addRoundedRect(x, y, w, h, w / 2, w / 2)
+
         percentage = self.currentPercentage
         animatedPath1 = self.trim(path, percentage)
         painter.drawPath(animatedPath1)
 
         point = path.pointAtPercent(.8)
-        painter.drawLine(point.x(), point.y(), point.x()-self.length, point.y()-self.length)
-        painter.drawLine(point.x(), point.y(), point.x()+self.length, point.y()-self.length)
+        painter.drawLine(point.x(), point.y(), point.x() - self.length,
+                         point.y() - self.length)
+        painter.drawLine(point.x(), point.y(), point.x() + self.length,
+                         point.y() - self.length)
 
-        percentage = self.currentPercentage-.5
+        percentage = self.currentPercentage - .5
         if percentage < 0:
             percentage += 1
         if percentage > 1:
@@ -130,14 +131,17 @@ class ReloadButton(QPushButton):
         painter.drawPath(animatedPath2)
 
         point = path.pointAtPercent(.3)
-        point.setX(point.x()+1)
-        painter.drawLine(point.x(), point.y(), point.x()-self.length, point.y()+self.length)
-        painter.drawLine(point.x(), point.y(), point.x()+self.length, point.y()+self.length)
+        point.setX(point.x() + 1)
+        painter.drawLine(point.x(), point.y(), point.x() - self.length,
+                         point.y() + self.length)
+        painter.drawLine(point.x(), point.y(), point.x() + self.length,
+                         point.y() + self.length)
 
         font = QFont()
         font.setPointSize(30)
         painter.setFont(font)
 
-        painter.drawText(0, 0, self.width()-30, self.height(), Qt.AlignRight|Qt.AlignVCenter, "Reload")
+        painter.drawText(0, 0, self.width() - 30, self.height(),
+                         Qt.AlignRight | Qt.AlignVCenter, "Reload")
 
         painter.end()
