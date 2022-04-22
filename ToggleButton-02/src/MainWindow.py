1b4f2063c952d5e199f29edf9f4e55aa835e3dfb
Parham Taheri <parhamoyan@yahoo.com>
2022-04-23 00:01:30 +0200

    refactor and add type annotation

--- ToggleButton-02/src/MainWindow.py
+++ ToggleButton-02/src/MainWindow.py
@@ -4,25 +4,27 @@
 # All rights reserved.
 #
 # ///////////////////////////////////////////////////////////////
+from typing import Optional
 
 from PySide6.QtGui import QCursor, QGuiApplication
 from PySide6.QtWidgets import QGridLayout, QMainWindow, QWidget
 from ToggleButton import ToggleButton
 
+
 class MainWindow(QMainWindow):
-    def __init__(self):
-        QMainWindow.__init__(self)
+    def __init__(self, parent: Optional[QWidget] = None) -> None:
+        QMainWindow.__init__(self, parent)
         self.setFixedSize(800, 600)
         self.setStyleSheet("background: #f6f6ff;")
         self.centralWidget = QWidget(self)
-        
+
         self.customCheckBox = ToggleButton(self.centralWidget)
         self.gridLayout = QGridLayout(self.centralWidget)
         self.gridLayout.addWidget(self.customCheckBox)
 
         self.setCentralWidget(self.centralWidget)
         self.center()
-    
+
     def center(self):
         screen = QGuiApplication.screenAt(QCursor().pos())
         fg = self.frameGeometry()
--- ToggleButton-02/src/ToggleButton.py
+++ ToggleButton-02/src/ToggleButton.py
@@ -4,115 +4,124 @@
 # All rights reserved.
 #
 # ///////////////////////////////////////////////////////////////
+from typing import Optional
 
 from PySide6.QtCore import *
 from PySide6.QtGui import *
 from PySide6.QtWidgets import *
 
+
 class ToggleButton(QCheckBox):
-    def __init__(
-            self,
-            parent = None,
-            leftIconColor = QColor("#ffffff"),
-            rightIconColor = QColor("#333333"),
-            animationDuration = 800
-            ):
-        QCheckBox.__init__(self, parent=parent)
+    def __init__(self, parent: Optional[QWidget] = None) -> None:
+        QCheckBox.__init__(self, parent)
         self.setCursor(Qt.PointingHandCursor)
         self.setFixedSize(200, 100)
         self.addShadow()
         self.stateChanged.connect(self.startAnimations)
         self.blackRectWidth = self.height()
         self.blackRectXPos = 0
-        self.leftIconColor = leftIconColor
-        self.rightIconColor = rightIconColor
-        self.animationDuration = animationDuration
+        self.leftIconColor = QColor("#ffffff")
+        self.rightIconColor = QColor("#333333")
+        self.animationDuration = 800
 
-    def newQVariantAnimation(self):
+    def newQVariantAnimation(self) -> QVariantAnimation:
         animation = QVariantAnimation(self)
         animation.setDuration(self.animationDuration)
         animation.setEasingCurve(QEasingCurve.InOutExpo)
         return animation
 
-    def initBlackRectWidthAnimation(self):
-        self.blackRectWidthAnimation = self.newQVariantAnimation()
-        self.blackRectWidthAnimation.setStartValue(self.blackRectWidth)
-        self.blackRectWidthAnimation.setEndValue([self.width(), self.height()][not self.isChecked()])
-        self.blackRectWidthAnimation.valueChanged.connect(self.updateBlackRectWidth)
-    
-    def initBlackRectXPosAnimation(self):
-        self.blackRectXPosAnimation = self.newQVariantAnimation()
-        self.blackRectXPosAnimation.setStartValue(self.blackRectXPos)
-        self.blackRectXPosAnimation.setEndValue([self.width()//2, 0][not self.isChecked()])
-        self.blackRectXPosAnimation.setDuration(self.animationDuration)
-        self.blackRectXPosAnimation.valueChanged.connect(self.updateBlackRectXPos)
+    def getBlackRectWidthAnimation(self) -> QVariantAnimation:
+        blackRectWidthAnimation = self.newQVariantAnimation()
+        blackRectWidthAnimation.setStartValue(self.blackRectWidth)
+        blackRectWidthAnimation.setEndValue(
+            [self.width(), self.height()][not self.isChecked()])
+        blackRectWidthAnimation.valueChanged.connect(
+            self.updateBlackRectWidth)
+        return blackRectWidthAnimation
 
-    def initLeftIconColorAnimation(self):
-        self.leftIconColorAnimation = QVariantAnimation(self)
-        self.leftIconColorAnimation.setStartValue(self.leftIconColor)
-        self.leftIconColorAnimation.setEndValue(QColor(["#333333", "#ffffff"][not self.isChecked()]))
-        self.leftIconColorAnimation.setDuration(self.animationDuration)
-        self.leftIconColorAnimation.setEasingCurve(QEasingCurve.InOutExpo)
-        self.leftIconColorAnimation.valueChanged.connect(self.updateLeftIconColor)
+    def getBlackRectXPosAnimation(self) -> QVariantAnimation:
+        blackRectXPosAnimation = self.newQVariantAnimation()
+        blackRectXPosAnimation.setStartValue(self.blackRectXPos)
+        blackRectXPosAnimation.setEndValue(
+            [self.width() // 2, 0][not self.isChecked()])
+        blackRectXPosAnimation.setDuration(self.animationDuration)
+        blackRectXPosAnimation.valueChanged.connect(
+            self.updateBlackRectXPos)
+        return blackRectXPosAnimation
 
-    def initRightIconColorAnimation(self):
-        self.rightIconColorAnimation = QVariantAnimation(self)
-        self.rightIconColorAnimation.setStartValue(self.rightIconColor)
-        self.rightIconColorAnimation.setEndValue(QColor(["#333333", "#ffffff"][self.isChecked()]))
-        self.rightIconColorAnimation.setDuration(self.animationDuration)
-        self.rightIconColorAnimation.setEasingCurve(QEasingCurve.InOutExpo)
-        self.rightIconColorAnimation.valueChanged.connect(self.updateRightIconColor)
+    def getLeftIconColorAnimation(self) -> QVariantAnimation:
+        leftIconColorAnimation = QVariantAnimation(self)
+        leftIconColorAnimation.setStartValue(self.leftIconColor)
+        leftIconColorAnimation.setEndValue(
+            QColor(["#333333", "#ffffff"][not self.isChecked()]))
+        leftIconColorAnimation.setDuration(self.animationDuration)
+        leftIconColorAnimation.setEasingCurve(QEasingCurve.InOutExpo)
+        leftIconColorAnimation.valueChanged.connect(
+            self.updateLeftIconColor)
+        return leftIconColorAnimation
 
-    def startAnimations(self):
-        self.setDisabled(True)
-        self.initBlackRectWidthAnimation()
-        self.initBlackRectXPosAnimation()
-        self.initLeftIconColorAnimation()
-        self.initLeftIconColorAnimation()
-        self.initRightIconColorAnimation()
+    def getRightIconColorAnimation(self) -> QVariantAnimation:
+        rightIconColorAnimation = QVariantAnimation(self)
+        rightIconColorAnimation.setStartValue(self.rightIconColor)
+        rightIconColorAnimation.setEndValue(
+            QColor(["#333333", "#ffffff"][self.isChecked()]))
+        rightIconColorAnimation.setDuration(self.animationDuration)
+        rightIconColorAnimation.setEasingCurve(QEasingCurve.InOutExpo)
+        rightIconColorAnimation.valueChanged.connect(
+            self.updateRightIconColor)
+        return rightIconColorAnimation
 
+    def startAnimations(self) -> None:
         parGroup1 = QParallelAnimationGroup(self)
-        parGroup1.addAnimation([self.blackRectXPosAnimation, self.blackRectWidthAnimation][self.isChecked()])
+        match self.isChecked():
+            case True:
+                parGroup1.addAnimation(self.getBlackRectWidthAnimation())
+            case False:
+                parGroup1.addAnimation(self.getBlackRectXPosAnimation())
 
         parGroup2 = QParallelAnimationGroup(self)
-        parGroup2.addAnimation([self.blackRectXPosAnimation, self.blackRectWidthAnimation][not self.isChecked()])  
-        parGroup2.addAnimation(self.leftIconColorAnimation)
-        parGroup2.addAnimation(self.rightIconColorAnimation)
+        match self.isChecked():
+            case True:
+                parGroup2.addAnimation(self.getBlackRectXPosAnimation())
+            case False:
+                parGroup2.addAnimation(self.getBlackRectWidthAnimation())
+        parGroup2.addAnimation(self.getLeftIconColorAnimation())
+        parGroup2.addAnimation(self.getRightIconColorAnimation())
 
         secGroup = QSequentialAnimationGroup(self)
         secGroup.addAnimation(parGroup1)
         secGroup.addAnimation(parGroup2)
         secGroup.finished.connect(lambda: self.setDisabled(False))
         secGroup.start()
-    
-    def updateBlackRectWidth(self, newValue):
+
+    def updateBlackRectWidth(self, newValue) -> None:
         self.blackRectWidth = newValue
         self.update()
 
-    def updateBlackRectXPos(self, newValue):
+    def updateBlackRectXPos(self, newValue) -> None:
         self.blackRectXPos = newValue
         self.update()
 
-    def updateLeftIconColor(self, newColor):
+    def updateLeftIconColor(self, newColor) -> None:
         self.leftIconColor = newColor
         self.update()
-    
-    def updateRightIconColor(self, newColor):
+
+    def updateRightIconColor(self, newColor) -> None:
         self.rightIconColor = newColor
         self.update()
 
-    def addShadow(self):
+    def addShadow(self) -> None:
         self.shadow = QGraphicsDropShadowEffect()
         self.shadow.setXOffset(3)
         self.shadow.setYOffset(3)
         self.shadow.setBlurRadius(80)
         self.shadow.setColor(QColor(0, 0, 0, 40))
         self.setGraphicsEffect(self.shadow)
-    
-    def hitButton(self, pos: QPoint):
+
+    def hitButton(self, pos: QPoint) -> None:
         return self.contentsRect().contains(pos)
 
-    def paintEvent(self, e):
+    def paintEvent(self, event: QPaintEvent) -> None:
         p = QPainter(self)
         p.setRenderHint(QPainter.Antialiasing)
         p.setPen(Qt.NoPen)
@@ -121,21 +130,24 @@ class ToggleButton(QCheckBox):
         p.setBrush(QBrush(QColor("white")))
         x, y = 0, 0
         w, h = self.width(), self.height()
-        radius = self.height()/2
+        radius = self.height() / 2
         p.drawRoundedRect(x, y, w, h, radius, radius)
-        
+
         # DRAW BLACK CIRCLE OR HORIZONTAL ROUNDED RECT
         p.setBrush(QBrush(QColor("#333333")))
-        p.drawRoundedRect(x+self.blackRectXPos, y, (self.blackRectWidth-self.blackRectXPos), \
-            self.height(), self.height()/2, self.height()/2)
+        p.drawRoundedRect(x + self.blackRectXPos, y,
+                          (self.blackRectWidth - self.blackRectXPos),
+                          self.height(), self.height() / 2, self.height() / 2)
 
         # DRAW WHITE CIRCLE
         p.setBrush(QBrush(self.leftIconColor))
         r1 = 24
-        p.drawEllipse(x+(self.height()-r1)/2, y+(self.height()-r1)/2, r1, r1)
+        p.drawEllipse(x + (self.height() - r1) / 2,
+                      y + (self.height() - r1) / 2, r1, r1)
 
         # DRAW VERTICAL BLACK ROUNDED RECT
         p.setBrush(QBrush(self.rightIconColor))
-        p.drawRoundedRect(x+self.width()*.75-6, y+(self.height()-40)/2, \
-             12, 40, 6, 6)
+        p.drawRoundedRect(x + self.width() * .75 - 6,
+                          y + (self.height() - 40) / 2,
+                          12, 40, 6, 6)
         p.end()
