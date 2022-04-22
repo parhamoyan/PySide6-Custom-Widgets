cc5f03d4a83d41c5240dbd7fa4453275d9827465
Parham Taheri <parhamoyan@yahoo.com>
2022-04-22 23:44:25 +0200

    refactor and and type annotation

--- ToggleButton-01/src/ToggleButton.py
+++ ToggleButton-01/src/ToggleButton.py
@@ -4,116 +4,109 @@
 # All rights reserved.
 #
 # ///////////////////////////////////////////////////////////////
+from typing import Optional
 
-from PySide6.QtCore import *
-from PySide6.QtGui import *
-from PySide6.QtWidgets import *
+from PySide6.QtGui import QColor, QPainterPath, QPen, QPainter
+from PySide6.QtCore import Qt, QPoint, QEasingCurve, QParallelAnimationGroup, \
+    QVariantAnimation
+from PySide6.QtWidgets import QCheckBox, QWidget
+
 
 class ToggleButton(QCheckBox):
-    def __init__(
-            self,
-            parent = None,
-            backgroundColor = QColor("#414141"),
-            indicatorColor = QColor("#ffffff"),
-            borderWidth = 24,
-            animationDuration = 800
-            ):
-        QCheckBox.__init__(self, parent=parent)
+    def __init__(self, parent: Optional[QWidget] = None) -> None:
+        QCheckBox.__init__(self, parent)
         self.setFixedSize(200, 120)
         self.setCursor(Qt.PointingHandCursor)
         # INIT ATTRIBUTES
         self.percentage = .75
-        self.animationDuration = animationDuration
-        self.backgroundColor = backgroundColor
-        self.indicatorColor = indicatorColor
-        self.borderWidth = borderWidth
-        # INIT ANIMATIONS
-        self.initTransitionAnimation()
-        self.initIndicatorColorAnimation()
+        self.animationDuration = 800
+        self.backgroundColor = QColor("#414141")
+        self.indicatorColor = QColor("#ffffff")
+        self.borderWidth = 24
         # CONNECT SIGNAL
         self.stateChanged.connect(self.startAnimation)
-    
-    def hitButton(self, pos: QPoint):
+
+    def hitButton(self, pos: QPoint) -> None:
         return self.contentsRect().contains(pos)
-    
-    def initIndicatorColorAnimation(self):
-        self.indicatorColorAnimation = self.getQVariantAnimation()
-        self.indicatorColorAnimation.valueChanged.connect(self.updateIndicatorColor)
-    
-    def reinitIndicatorColorAnimation(self):
-        self.indicatorColorAnimation.setStartValue([QColor("#686868"), QColor("#ffffff")][self.isChecked()])
-        self.indicatorColorAnimation.setEndValue([QColor("#686868"), QColor("#ffffff")][not self.isChecked()])
-    
-    def updateIndicatorColor(self, newColor):
+
+    def getIndicatorColorAnimation(self) -> QVariantAnimation:
+        indicatorColorAnimation = self.getQVariantAnimation()
+        indicatorColorAnimation.valueChanged.connect(self.updateIndicatorColor)
+        match self.isChecked():
+            case True:
+                indicatorColorAnimation.setStartValue(QColor("#ffffff"))
+                indicatorColorAnimation.setEndValue(QColor("#686868"))
+            case False:
+                indicatorColorAnimation.setStartValue(QColor("#ffffff"))
+                indicatorColorAnimation.setEndValue(QColor("#686868"))
+        return indicatorColorAnimation
+
+    def updateIndicatorColor(self, newColor) -> None:
         self.indicatorColor = newColor
         self.update()
-    
-    def getQVariantAnimation(self):
+
+    def getQVariantAnimation(self) -> QVariantAnimation:
         animation = QVariantAnimation(self)
         animation.setEasingCurve(QEasingCurve.OutBack)
         animation.setDuration(self.animationDuration)
         return animation
-    
-    def initTransitionAnimation(self):
-        self.transitionAnimation = self.getQVariantAnimation()
-        self.transitionAnimation.valueChanged.connect(self.updatePercentage)
-    
-    def reinitTransitionAnimation(self):
-        self.transitionAnimation.setStartValue(self.percentage)
-        self.transitionAnimation.setEndValue(self.percentage+.5)
 
-    def updatePercentage(self, newValue):
+    def getTransitionAnimation(self) -> QVariantAnimation:
+        transitionAnimation = self.getQVariantAnimation()
+        transitionAnimation.valueChanged.connect(self.updatePercentage)
+        transitionAnimation.setStartValue(self.percentage)
+        transitionAnimation.setEndValue(self.percentage + .5)
+        return transitionAnimation
+
+    def updatePercentage(self, newValue) -> None:
         self.percentage = newValue
         if self.percentage > 1:
             self.percentage -= 1
         self.update()
 
-    def startAnimation(self, state):
-        self.setDisabled(True)
-        self.reinitTransitionAnimation()
-        self.reinitIndicatorColorAnimation()
+    def startAnimation(self) -> None:
         parGroup = QParallelAnimationGroup(self)
-        parGroup.addAnimation(self.transitionAnimation)
-        parGroup.addAnimation(self.indicatorColorAnimation)
-        parGroup.finished.connect(lambda: self.setDisabled(False))
+        parGroup.addAnimation(self.getTransitionAnimation())
+        parGroup.addAnimation(self.getIndicatorColorAnimation())
         parGroup.start()
-    
-    def drawAnimatedPath(self):
-        self.animatedPath = QPainterPath()
+
+    def drawAnimatedPath(self, painter: QPainter,
+                         backgroundPath: QPainterPath) -> None:
+        animatedPath = QPainterPath()
         percentage = self.percentage
-        self.animatedPath.moveTo(self.backgroundPath.pointAtPercent(percentage))
-        for i in range(1, self.width()+1):
-            self.animatedPath.lineTo(self.backgroundPath.pointAtPercent(percentage))
-            percentage += .5/self.width()
+        animatedPath.moveTo(backgroundPath.pointAtPercent(percentage))
+        for i in range(1, self.width() + 1):
+            animatedPath.lineTo(backgroundPath.pointAtPercent(percentage))
+            percentage += .5 / self.width()
             if percentage > 1:
                 percentage -= 1
-        self.painter.drawPath(self.animatedPath)
+        painter.drawPath(animatedPath)
 
-    def paintEvent(self, e):
+    def paintEvent(self, event: QPainter) -> None:
         # INIT PEN AND BACKGROUND COLOR
         pen = QPen()
         pen.setWidth(self.borderWidth)
         pen.setColor(self.backgroundColor)
 
         # INIT PAINTER
-        self.painter = QPainter(self)
-        self.painter.setRenderHint(QPainter.Antialiasing)
-        self.painter.setPen(pen)
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.setPen(pen)
 
         # INIT & DRAW BACKGROUND PATH
-        self.backgroundPath = QPainterPath()
+        backgroundPath = QPainterPath()
         x, y = pen.width(), pen.width()
-        w, h = self.width()-pen.width()*2, self.height()-pen.width()*2
-        self.backgroundPath.addRoundedRect(x, y, w, h, h/2, h/2)
-        self.painter.drawPath(self.backgroundPath)
+        w, h = self.width() - pen.width() * 2, self.height() - pen.width() * 2
+        backgroundPath.addRoundedRect(x, y, w, h, h / 2, h / 2)
+        painter.drawPath(backgroundPath)
 
         # SET INDICATOR COLOR
         pen.setColor(self.indicatorColor)
         pen.setCapStyle(Qt.RoundCap)
-        self.painter.setPen(pen)
+        painter.setPen(pen)
 
         # DRAW ANIMATED PATH
-        self.drawAnimatedPath()
-        
+        self.drawAnimatedPath(painter, backgroundPath)
+
         # END PAINTER
-        self.painter.end()
+        painter.end()
--- ToggleButton-01/src/main.py
+++ ToggleButton-01/src/main.py
@@ -16,4 +16,4 @@ if __name__ == "__main__":
     window = MainWindow()
     window.show()
 
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
